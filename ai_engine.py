7318ae868f43cddaf4d793dfaab8cf421426e345
Prabhgun Sobti <prabhgunsinghsobti@gmail.com>
2026-06-02 22:38:06 +0530

    Change model from 'gemini-2.0-flash' to 'gemini-1.5-flash'

--- ai_engine.py
+++ ai_engine.py
@@ -5,10 +5,11 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 load_dotenv()
 
 llm = ChatGoogleGenerativeAI(
-    model="gemini-2.0-flash",
+    model="gemini-1.5-flash",
     google_api_key=os.getenv("GOOGLE_API_KEY"),
     temperature=0.7
 )
+
 def chat(user_input):
     return llm.invoke(user_input).content
 
